d04a7444d63ebf94af9a01730ef85e11e0d9254f
sahil-sahu <sahilku2003@gmail.com>
2024-06-05 16:58:29 +0530

    done with docker bug fixes

--- backend/src/helpers/formatwRedis.ts
+++ backend/src/helpers/formatwRedis.ts
@@ -2,79 +2,75 @@ import { prisma } from '../prisma';
 import { redisClient } from '../redis';
 import { AuthRequest, Patientlog, FullLog, NotificationLoad } from '../types';
 
-export const fireTokensFromICU = async(icuId:number):Promise<string[]>=>{
+export const fireTokensFromICU = async (icuId: number): Promise<string[]> => {
     const icus = await redisClient.get(`ICU:${icuId}`);
-    if(!icus){
+    if (!icus) {
         const watchers = await prisma.watcher.findMany({
-            where:{icuId},
-            include:{
-                user:{
-                    select:{
-                        fireTokens:true
+            where: { icuId },
+            include: {
+                user: {
+                    select: {
+                        fireTokens: true
                     }
                 }
             }
         });
-        // const users = await prisma.user.findMany({where:{id: {in:watchers.map(watcher => watcher.userid)}}})
-        const fireTokens = watchers.flatMap(watcher => watcher.user.fireTokens);
-        const savetoRedis = await redisClient.set(`ICU:${icuId}`, JSON.stringify(fireTokens));
-        await redisClient.expire(`ICU:${icuId}`, 60*60*6);
-        return fireTokens;
 
+        const fireTokens = watchers.flatMap((watcher: { user: { fireTokens: string[] } }) => watcher.user.fireTokens);
+        await redisClient.set(`ICU:${icuId}`, JSON.stringify(fireTokens));
+        await redisClient.expire(`ICU:${icuId}`, 60 * 60 * 6);
+        return fireTokens;
     }
     return JSON.parse(icus);
 }
 
-const callFromDB = async(log:Patientlog):Promise<[FullLog, NotificationLoad]> => {
+const callFromDB = async (log: Patientlog): Promise<[FullLog, NotificationLoad]> => {
     const res = await prisma.sensor.findUniqueOrThrow({
-        where:{
-            id:log.sensorid
+        where: {
+            id: log.sensorid
         },
-        include:{
-            bed:{
-                include:{
-                    patient:{
-                        select:{
-                            id:true,
-                            name:true
+        include: {
+            bed: {
+                include: {
+                    patient: {
+                        select: {
+                            id: true,
+                            name: true
                         }
                     },
-                    ICU:{
-                        select:{
-                            id:true,
-                            name:true
+                    ICU: {
+                        select: {
+                            id: true,
+                            name: true
                         }
                     }
                 }
             }
         }
     });
-    if(res.bedID == null)
+    if (res.bedID == null)
         throw "Sensor not connected to bed";
-    const {bedID,bed:{icuId,patientId,name:bedName,ICU:{name:icuName}}} = res;
-    // res.bed.ICU.
-    await redisClient.set(`sensor:${log.sensorid}`, JSON.stringify({patientId,bedID,icuId, bedName,icuName}))
-    await redisClient.expire(`sensor:${log.sensorid}`, 60*60*6);
-    return [{
-        ...log, patientId,bedID
-    },{patientId,bedID,icuId, bedName,icuName}]
+    const { bedID, bed: { icuId, patientId, name: bedName, ICU: { name: icuName } } } = res;
 
+    await redisClient.set(`sensor:${log.sensorid}`, JSON.stringify({ patientId, bedID, icuId, bedName, icuName }));
+    await redisClient.expire(`sensor:${log.sensorid}`, 60 * 60 * 6);
+    return [{
+        ...log, patientId, bedID
+    }, { patientId, bedID, icuId, bedName, icuName }];
 }
 
-
-
-export const reformat = async (log:Patientlog):Promise<[FullLog, NotificationLoad]> => {
+export const reformat = async (log: Patientlog): Promise<[FullLog, NotificationLoad]> => {
     const info = await redisClient.get(`sensor:${log.sensorid}`);
-    if(info){
+    if (info) {
         const infoDict = JSON.parse(info.toString());
         const patientId = infoDict.patientId;
         const bedID = infoDict.bedID;
         const icuId = infoDict.icuId;
-        if(patientId == null || bedID == null || icuId == null)
+        if (patientId == null || bedID == null || icuId == null)
             return await callFromDB(log);
         return [{
-            ...log, patientId,bedID
-        },infoDict]
+            ...log, patientId, bedID
+        }, infoDict];
     }
     return await callFromDB(log);
 }
