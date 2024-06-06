b2c62b3111c669075d1f12b222ad781829b64354
sahil-sahu <sahilku2003@gmail.com>
2024-06-06 17:44:10 +0530

    minor

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -6,7 +6,7 @@ let socket: Socket | null = null;
 
 export const connectToSocket = (room:string, setMessages:((message:Patientlog) => void) | null) => {
   
-  socket = socket?socket:io('http://localhost:8000');
+  socket = socket?socket:io(process.env.NEXT_PUBLIC_EVENT_URL || 'http://localhost:8000');
   socket.on('connect', () => {
     console.log('Connected to Socket.IO server');
   });
