37bbc778b53452acf8c8f6ddf37082627433276f
sahil-sahu <sahilku2003@gmail.com>
2024-06-11 22:27:29 +0530

    icu menu bar

--- frontend/src/app/glance/page.tsx
+++ frontend/src/app/glance/page.tsx
@@ -9,12 +9,29 @@ import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { GlanceInfo } from "@/types/glance";
 import useStack from "./hooks/useStack";
 import { connectToSocket, unsubscribeFromRoom } from "@/lib/socket";
-
+import { useSearchParams } from "next/navigation";
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+  } from "@/components/ui/navigation-menu"
+  import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
+import Link from "next/link";
 const AtGlance = () =>{
     const {data,isLoading,error, refetch} = useQuery({queryKey:['glance'], queryFn: fetchGlance})
     const {glances, setGlances, pushMessage} = useStack();
     const [_refresh,refresh] = useState(new Date().getMilliseconds());
     const mysocket = useRef<{room:string; unsubscribe: () => void;}[] | null>(null);
+
+    const searchParams = useSearchParams();
+    const icu = searchParams.get('icu') ?? "";
+    let icuId = !isNaN(parseInt(icu))?parseInt(icu): -1;
+    if(data) {icuId = !isNaN(parseInt(icu))?parseInt(icu):data[0].icu.id;}
     useLayoutEffect(()=>{
         var store = localStorage.getItem('glance');
         if(!store){
@@ -25,21 +42,25 @@ const AtGlance = () =>{
         if(store && data){
             const glances:string[] = JSON.parse(store);
             const arr:GlanceInfo[] = [];
-            data.beds.forEach(e => {
+            const icuId = !isNaN(parseInt(icu))?parseInt(icu):data[0].icu.id;
+            const temp = data.find(e => e.icu.id == icuId);
+            if(!temp) return;
+            const {icu:{beds}} = temp;
+            beds.forEach(e => {
                 if(glances.includes(e.patientId)){
                     e.pinned = true;
                     arr.push(e);
                 }
             })
-            data.beds.forEach(e => {
+            beds.forEach(e => {
                 if(!glances.includes(e.patientId)){
                     e.pinned = false;
                     arr.push(e);
                 }
             })
-            setGlances(arr);
+            setGlances(arr.filter(e => e.icuId == icuId));
         }
-    },[_refresh, data])
+    },[_refresh, data, icu])
     useEffect(()=>{
         if(!data) return;
         const connectRealtime = (icuId:string)=>{
@@ -52,7 +73,7 @@ const AtGlance = () =>{
             };
         }
 
-       if(mysocket.current == null)  mysocket.current = data?.icus.map(e => connectRealtime(e));
+       if(mysocket.current == null)  mysocket.current = data?.map(e => connectRealtime(e.icu.id.toString()));
     },[data, mysocket])
     if(isLoading){
         return(
@@ -67,9 +88,25 @@ const AtGlance = () =>{
         </main>
         )
     }
+    // if(data) console.log(icu, data[0].icu.id.toString() , icu == data[0].icu.id.toString());
     return(
         <main>
             <NavBox title={"At a Glance"} />
+            <NavigationMenu className="m-auto p-4">
+                <NavigationMenuList>
+                    {
+                        data?.map(({icu:{name, id}}) => (
+                            <NavigationMenuItem className={icuId == id? "border-2 border-gray-300 rounded":""} key={id}>
+                                <Link href={`/glance?icu=${id}`} legacyBehavior passHref>
+                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                        {name}
+                                    </NavigationMenuLink>
+                                </Link>
+                            </NavigationMenuItem>
+                        ))
+                    }
+                </NavigationMenuList>
+            </NavigationMenu>
             <section className="grid md:grid-cols-3 grid-cols-2">
                 {
                     glances && glances.map(cell => <GlanceBox refresh={refresh} key={cell.id} pinned={cell.pinned?true:false} data={cell}/>)
--- frontend/src/app/glance/query/query.ts
+++ frontend/src/app/glance/query/query.ts
@@ -1,10 +1,13 @@
 import { axiosInstance, setheader } from "@/lib/axios";
 import { GlanceInfo } from "@/types/glance";
 interface glanceOp {
-  beds : GlanceInfo[];
-  icus : string[];
+  icu: {
+    id: number;
+    name: string;
+    beds: GlanceInfo[]
+  }
 }
-export const fetchGlance = async (): Promise<glanceOp> => {
+export const fetchGlance = async (): Promise<glanceOp[]> => {
     const response = await axiosInstance.get(`/hospital/glance`, {
         headers: await setheader(),
       });
