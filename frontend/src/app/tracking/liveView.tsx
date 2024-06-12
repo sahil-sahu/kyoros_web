fb042ed023abf0e6f6728ae747752d5cfada5dad
sahil-sahu <sahilku2003@gmail.com>
2024-06-12 17:12:18 +0530

    loading to liveView

--- frontend/src/app/tracking/liveView.tsx
+++ frontend/src/app/tracking/liveView.tsx
@@ -43,18 +43,36 @@ const LiveView =({patientId}:{patientId:string|null}) =>{
     }
 
     if(logLoading){
-        return <div className="container shadow-inner max-w-lg my-4 p-3 flex flex-col justify-evenly rounded-lg" style={liveBox}>
-                    <div className="w-full flex justify-evenly items-center p-2">
-                        <Skeleton className="h-12 w-12 rounded-full" />
-                        <div className='flex flex-col gap-3 justify-evenly items-center'>
-                            <Skeleton className="h-4 w-[250px]" />
-                            <div className='flex justify-between gap-3'>
-                                <Skeleton className="h-4 w-[200px]" />
-                                <Skeleton className="h-4 w-[200px]" />
-                            </div>
+        return (
+            <div className='grid lg:grid-cols-2 grid-cols-1 max-w-5xl m-auto gap-4'>
+            <div style={{
+                background:"linear-gradient(to bottom right, #303778, #4C8484)"
+            }} className='text-white flex justify-evenly rounded-xl p-4 py-8'>
+                <div className="flex flex-col justify-evenly items-center p-2 w-[40%] ">
+                    <Skeleton className="h-12 w-12 rounded-full" />
+                    <div className='flex flex-col justify-evenly items-center gap-5'>
+                        <h3 className='text-lg'>
+                            <Skeleton className="h-4 w-[200px]" />
+                        </h3>
+                        <div className='flex justify-between gap-3'>
+                            <Skeleton className="h-4 w-[80px]" />
+                            <Skeleton className="h-4 w-[80px]" />
                         </div>
                     </div>
-                    <ul className={`grid grid-cols-3 grid-flow-row gap-2 ${styles.PatientInfo}`}>
+                </div>
+                <div className='divider border-white border h-full'></div>
+                <div className='p-3 py-5 w-[40%] text-center'>
+                    <h3 className='my-3 text-lg font-semibold'>
+                        Diagnosis
+                    </h3>
+                    <Skeleton className="h-[100px] w-[200px]" />
+                </div>
+            </div>
+            <div className='p-5 border-2 rounded-xl border-darkblue'>
+                <h3 className='text-center text-2xl font-bold'>
+                    Vitals
+                </h3>
+                <ul className={`grid grid-cols-3 grid-flow-row gap-2 ${styles.PatientInfo}`}>
                         <li>
                         <Skeleton>
                             <h4>&nbsp;</h4>
@@ -86,7 +104,9 @@ const LiveView =({patientId}:{patientId:string|null}) =>{
                         </Skeleton>
                         </li>
                     </ul>
+            </div>
         </div>
+        )
     }
 
     if(err){
@@ -151,48 +171,6 @@ Presents to ED with a 2 day H/O high fever, headache, & Rt sided Facial swelling
                     </li>
                 </ul>
             </div>
-        </div>
-    )
-
-    return(
-        <div className="container shadow-inner max-w-lg my-4 p-3 flex flex-col justify-evenly rounded-lg" style={liveBox}>
-            <div className="w-full flex justify-evenly items-center p-2">
-                <Avatar style={{height:"100px", width:"100px"}}>
-                    <AvatarImage src="https://github.com/shadcn.png" />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <div className='flex flex-col justify-evenly items-center'>
-                    <h3 className='text-lg'>
-                        {logs?.name}
-                    </h3>
-                    <div className='flex justify-between gap-3'>
-                        <span>{`Age: ${logs?.age}`}</span>
-                        <span>{logs?.gender}</span>
-                    </div>
-                </div>
-            </div>
-            <ul className={`grid grid-cols-3 grid-flow-row gap-2 ${styles.PatientInfo}`}>
-                <li>
-                    <h4>7</h4>
-                    <small>Days in ICU</small>
-                </li>
-                <li>
-                    <h4>{latestInfo?.spo2}</h4>
-                    <small>SpO2</small>
-                </li>
-                <li>
-                    <h4>{`${latestInfo?.bp[0]}/${latestInfo?.bp[1]}`}</h4>
-                    <small>Blood Pressure</small>
-                </li>
-                <li>
-                    <h4>{latestInfo?.heart_rate}</h4>
-                    <small>Heart Rate</small>
-                </li>
-                <li>
-                    <h4>{latestInfo?.temp}</h4>
-                    <small>Temperature</small>
-                </li>
-            </ul>
         </div>
     )
 }
